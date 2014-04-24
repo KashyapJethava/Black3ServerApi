4e37887f13eae5909d9ac3c48cc7cd48e8847079
Kashyap Jethava <kashyap.jethava@gmail.com>
2014-04-24 18:30:30 +0530

    lets get started....

--- Black3ClassLibrary/Common/Deck.cs
+++ Black3ClassLibrary/Common/Deck.cs
@@ -8,63 +8,63 @@ using Black3ClassLibrary.Enums;
 namespace Black3ClassLibrary.Common
 {
     public static class Deck
-    {
-        public static readonly List<string> deck = new List<string>();
-
+    {   
         public static List<string> GetOrderedDeck()
         {
-            deck[0] = "2C";
-            deck[1] = "2S";
-            deck[2] = "2H";
-            deck[3] = "2D";
-            deck[4] = "3C";
-            deck[5] = "3S";
-            deck[6] = "3H";
-            deck[7] = "3D";
-            deck[8] = "4C";
-            deck[9] = "4S";
-            deck[10] = "4H";
-            deck[11] = "4D";
-            deck[12] = "5C";
-            deck[13] = "5S";
-            deck[14] = "5H";
-            deck[15] = "5D";
-            deck[16] = "6C";
-            deck[17] = "6S";
-            deck[18] = "6H";
-            deck[19] = "6D";
-            deck[20] = "7C";
-            deck[21] = "7S";
-            deck[22] = "7H";
-            deck[23] = "7D";
-            deck[24] = "8C";
-            deck[25] = "8S";
-            deck[26] = "8H";
-            deck[27] = "8D";
-            deck[28] = "9C";
-            deck[29] = "9S";
-            deck[30] = "9H";
-            deck[31] = "9D";
-            deck[32] = "10C";
-            deck[33] = "10S";
-            deck[34] = "10H";
-            deck[35] = "10D";
-            deck[36] = "JC";
-            deck[37] = "JS";
-            deck[38] = "JH";
-            deck[39] = "JD";
-            deck[40] = "QC";
-            deck[41] = "QS";
-            deck[42] = "QH";
-            deck[43] = "QD";
-            deck[44] = "KC";
-            deck[45] = "KS";
-            deck[46] = "KH";
-            deck[47] = "KD";
-            deck[48] = "AC";
-            deck[49] = "AS";
-            deck[50] = "AH";
-            deck[51] = "AD";
+            List<string> deck = new List<string>();
+            
+            deck.Add("2C");
+            deck.Add("2D");
+            deck.Add("2S");
+            deck.Add("2H");
+            deck.Add("3C");
+            deck.Add("3S");
+            deck.Add("3H");
+            deck.Add("3D");
+            deck.Add("4C");
+            deck.Add("4S");
+            deck.Add("4H");
+            deck.Add("4D");
+            deck.Add("5C");
+            deck.Add("5S");
+            deck.Add("5H");
+            deck.Add("5D");
+            deck.Add("6C");
+            deck.Add("6S");
+            deck.Add("6H");
+            deck.Add("6D");
+            deck.Add("7C");
+            deck.Add("7S");
+            deck.Add("7H");
+            deck.Add("7D");
+            deck.Add("8C");
+            deck.Add("8S");
+            deck.Add("8H");
+            deck.Add("8D");
+            deck.Add("9C");
+            deck.Add("9S");
+            deck.Add("9H");
+            deck.Add("9D");
+            deck.Add("10C");
+            deck.Add("10S");
+            deck.Add("10H");
+            deck.Add("10D");
+            deck.Add("JC");
+            deck.Add("JS");
+            deck.Add("JH");
+            deck.Add("JD");
+            deck.Add("QC");
+            deck.Add("QS");
+            deck.Add("QH");
+            deck.Add("QD");
+            deck.Add("KC");
+            deck.Add("KS");
+            deck.Add("KH");
+            deck.Add("KD");
+            deck.Add("AC");
+            deck.Add("AS");
+            deck.Add("AH");
+            deck.Add("AD");
 
             return deck;
         }
